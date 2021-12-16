52ba82d78b171df23da4790a57a716a2cdab5c4f
Glasovac <josip.glasovac@cinnamon.agency>
2021-12-16 16:27:50 +0100

    Fastlane config, fastlane screenshots

--- FastlaneTestProjectUITests/FastlaneTestProjectUITests.swift
+++ FastlaneTestProjectUITests/FastlaneTestProjectUITests.swift
@@ -9,34 +9,19 @@ import XCTest
 
 class FastlaneTestProjectUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    override func setUp() {
         let app = XCUIApplication()
+        setupSnapshot(app)
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    func test_screenshot_ui() {
+        let app = XCUIApplication()
+        
+        let helloWorldStaticText = app.staticTexts["Hello, world!"]
+
+        snapshot("Screenshot1")
+
+        XCTAssertEqual(helloWorldStaticText.label, "Hello, world!")
     }
 }
